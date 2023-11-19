e50e6c0dfc424b7c1ac5392ed169d0f7f17292c3
ymgn <ymgn.dev@gmail.com>
2023-11-19 17:32:20 +0900

    fix: -y option

--- src/utils/prepare-for-installation.ts
+++ src/utils/prepare-for-installation.ts
@@ -16,7 +16,7 @@ export const prepareForInstallation = async () => {
         sudo -S apt autoclean -y
         sudo -S apt install -y git curl wget software-properties-common
         # Necessary for Pano
-        sudo -S apt install gir1.2-gda-5.0 gir1.2-gsound-1.0
+        sudo -S apt install -y gir1.2-gda-5.0 gir1.2-gsound-1.0
         `,
         { shell: true },
       ),
